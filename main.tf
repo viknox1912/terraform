a35e6881f7433f3ef5b47d6ae6fbce6651b7f87f
viknox1912 <viknox1912@gmail.com>
2022-11-14 14:10:45 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("divine-apogee-368514-9031380e973f.json")
+
+  project = "divine-apogee-368514"
+  region  = "europe-west"
+  zone    = "europe-west3-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
