8833205cea4fdf23e3349cebc57d93c22913be1a
viknox1912 <viknox1912@gmail.com>
2022-11-18 13:45:09 +0200

    nw stuff

--- main.tf
+++ main.tf
@@ -17,9 +17,33 @@ provider "google" {
 
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
-  auto_create_subnetworks = "true"
-  routing_mode            = "GLOBAL"
+  auto_create_subnetworks = "false"
+  delete_default_routes_on_create = true
+}
+resource "google_compute_subnetwork" "some_sub" {
+  name = "some-sub"
+  ip_cidr_range = "10.2.420.0/16"
+  network = google_compute_network.vpc_network.self_link
   
+}
+resource "google_compute_route" "some_route" {
+  name = "some-route"
+  dest_range = "0.0.0.0/0"
+  network = google_compute_network.vpc_network.self_link
+  next_next_hop_gateway = "default-internet-gateway"
+  priority = 100  
+}
+resource "google_compute_router" "some_router" {
+  name = "some-router"
+  network = google_compute_network.vpc_network.self_link
+  
+}
+resource "google_compute_router_nat" "some-nat" {
+  name = "some-router-nat"
+  router = google_compute_router.some_router.name
+  region = google_compute_router.some_router.region
+  nnat_ip_allocate_option = "AUTO_ONLY"
+  sousource_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"  
 }
 
 resource "google_compute_instance" "some_try_instance" {
